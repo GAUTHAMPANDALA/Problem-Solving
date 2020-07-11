903577d08e6f24ba0fa20819d57062457d25f3c1
GAUTHAMPANDALA <41846057+GAUTHAMPANDALA@users.noreply.github.com>
2020-07-11 14:20:10 +0530

    Add files via upload

---
+++ Divide and Conqor/PeakElementusingDivideandconqor.cpp
@@ -0,0 +1,57 @@
+// Given an array of integers. Find a peak element in it. An array element is a peak if it is NOT smaller than its neighbours.
+//  For corner elements, we need to consider only one neighbour.
+// We solve this using the Divide and Conqour Process.
+
+// In this Algorithm
+// Step-1: check for extreme conditions ie, at i=0 or i=n-1
+// Step-2: Check for termination condiion, when should the divide and conquor be stoped or what to achieve solution to stop it
+// Step-3: Dividing Step based on conditions.
+// Step-4: Combining the solution.
+
+#include<stdio.h>
+
+int peakelement(int a[],int low,int high,int n)
+{
+	
+	if(high>=low)
+	{
+		int mid= (low+high)/2;
+		
+		if((mid==0 && a[mid]>a[mid+1]) || (mid==0 && a[mid]>a[mid+1])) // Checking the extreme conditions in the array.
+		{
+			return mid;
+		}
+		
+		if((a[mid]>=a[mid+1]) && (a[mid]>=a[mid-1]))      // exit condition.
+		{
+			return mid;
+		}
+		
+		if(a[mid]<a[mid-1] && mid>0)		             // traverse left if a[mid-1]>a[mid]
+		{
+			peakelement(a,low,mid-1,n);
+		}
+		
+		peakelement(a,mid+1,high,n);  		        	// traverse left if a[mid+1]>a[mid]
+		 	
+	}
+
+
+}
+
+
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int a[n];
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	
+
+    int k=peakelement(a,0,n,n);
+    
+    printf("The peak element is: %d",a[k]);
+}
