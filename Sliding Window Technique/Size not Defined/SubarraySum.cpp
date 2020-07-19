26ec954d42f965a2cdda2efbb03d94ef46155116
GAUTHAMPANDALA <41846057+GAUTHAMPANDALA@users.noreply.github.com>
2020-07-19 22:53:54 +0530

    Update SubarraySum.cpp

--- Sliding Window Technique/Size not Defined/SubarraySum.cpp
+++ Sliding Window Technique/Size not Defined/SubarraySum.cpp
@@ -7,11 +7,12 @@ using namespace std;
 
 int subArraySum(int arr[], int n, int sum)
 {
-    /* Initialize curr_sum as value of first element and starting point as 0 */
-    int curr_sum = arr[0], start = 0, i;
+     int curr_sum = 0, start = 0, i;
 
     /* Add elements one by one to curr_sum and if the curr_sum exceeds the sum,then remove starting element */
-    for (i = 1; i <= n; i++) {
+    for (i = 0; i <= n; i++) {
+    	
+    	 curr_sum = curr_sum + arr[i];
         // If curr_sum exceeds the sum, then remove the starting elements 
         // This is the termination/Decrementing Statement.
         while (curr_sum > sum && start < i - 1) {
@@ -21,13 +22,12 @@ int subArraySum(int arr[], int n, int sum)
 
         // If curr_sum becomes equal to sum, then return true
         if (curr_sum == sum) {
-            cout << "Sum found between indexes "<< start << " and " << i - 1;
+            cout << "Sum found between indexes "<< start << " and " << i;
             return 1;
         }
 
         // Add this element to curr_sum
-        if (i < n)
-            curr_sum = curr_sum + arr[i];
+       
     }
 
     // If we reach here, then no subarray
