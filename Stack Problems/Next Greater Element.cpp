f26dd9d895b348fccfa1ab654e6d93a755738050
GAUTHAMPANDALA <41846057+GAUTHAMPANDALA@users.noreply.github.com>
2020-07-28 11:04:41 +0530

    Update Next Greater Element.cpp

--- Stack Problems/Next Greater Element.cpp
+++ Stack Problems/Next Greater Element.cpp
@@ -7,24 +7,35 @@
 using namespace std; 
 
 int calculateNextGreatElement(int a[],int n){
-stack < int > s; 
-  s.push(a[0]); 
+
+   stack < int > s; 
+   int l[n],j=0; // L[J] is array to store the next greater elements according to their index.
+   s.push(a[0]); 
     for (int i = 1; i < n; i++) {   
-    if (s.empty()) { 			 						  // If stak empty push, as to compare st[top] elemnt with rest of eleemts.
+    if (s.empty()) { 			 			// If stack empty push, as to compare st[top] elemnt with rest of eleemts.
       s.push(a[i]); 
       continue; 
     }   
-    while (s.empty() == false &&  a[i]> s.top()){ 		// if stack is not empty, and st[top]<a[i] then print st[top] and pop() top element.
-        cout << s.top() << " --> " << a[i] << endl; 
+    while (s.empty() == false &&  a[i]<cs.top()){ 		// if stack is not empty, and st[top]<a[i] then print st[top] and pop() top element.
+       l[j]=a[i];
+       // cout << s.top() << " --> " <<l[j] << endl; 
+        j++;
         s.pop(); 
     }  
-    s.push(a[i]);   									// 	// Push the index into stack as it is already processed.
+    s.push(a[i]);   						// Push the index into stack as it is already processed.
   } 
 
   while (s.empty() == false) {
-    cout << s.top() << " --> " << -1 << endl;  		  // As the top elemnts do not contain next higher digits, then print -1.
-    s.pop(); 
+  	l[j]=-1;
+     // cout << s.top() << " --> " << l[j]<< endl;  		  // As the top elemnts do not contain next higher digits, then print -1.
+    j++;s.pop(); 
+  }
+  
+  for(int j=0;j<n;j++)
+  {
+  	cout<<l[j]<<" ";
   } 
+	
 }
 
   
