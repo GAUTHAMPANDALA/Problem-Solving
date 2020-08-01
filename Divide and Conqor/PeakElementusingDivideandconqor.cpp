7e063318337c1614d177d0585b02ad8526312d1f
GAUTHAMPANDALA <41846057+GAUTHAMPANDALA@users.noreply.github.com>
2020-08-01 14:26:28 +0530

    Update PeakElementusingDivideandconqor.cpp

--- Divide and Conqor/PeakElementusingDivideandconqor.cpp
+++ Divide and Conqor/PeakElementusingDivideandconqor.cpp
@@ -12,36 +12,38 @@
 
 int peakelement(int a[],int low,int high,int n)
 {
-	
-     if(a[0]>a[1])
-	{
-	  return a[0];
-	}	
-     else if(high>=low)
+	 if(a[0]>a[1])
+		{
+			return 0;
+		}	
+	else if(high>=low)
 	{
 		int mid= (low+high)/2;
+		
+	
 		
 		if((mid==0 && a[mid]>a[mid+1])) // Checking the extreme conditions in the array.
 		{
 			return mid;
 		}
 		
-		if((a[mid]>=a[mid+1]) && (a[mid]>=a[mid-1]))      // exit condition.
+    	else if((a[mid]>=a[mid+1]) && (a[mid]>=a[mid-1]))      // exit condition.
 		{
 			return mid;
 		}
 		
-		if(mid==n-1 && a[mid]>a[mid-1])
-                  {
+		else if(mid==n-1 && a[mid]>a[mid-1])
+        {
         	return mid;
 		}
 	    // if(mid>0 && a[mid-1]>a[mid])
 		//{
 		peakelement(a,low,mid-1,n);
 	   // }
-		peakelement(a,mid+1,high,n);  		        	
+		peakelement(a,mid+1,high,n);  		        	// traverse right if a[mid+1]>a[mid]
 		 	
 	}
+
 
 
 }
