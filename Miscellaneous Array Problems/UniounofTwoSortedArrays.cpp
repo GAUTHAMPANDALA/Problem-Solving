0d0d04dd9f4666590d206144a2b1bcfff249dd8d
GAUTHAMPANDALA <41846057+GAUTHAMPANDALA@users.noreply.github.com>
2020-08-03 15:56:35 +0530

    Update UniounofTwoSortedArrays.cpp

--- Miscellaneous Array Problems/UniounofTwoSortedArrays.cpp
+++ Miscellaneous Array Problems/UniounofTwoSortedArrays.cpp
@@ -1,12 +1,14 @@
 							//   Unioun of Two Sorted Arrays //
-	       // Given a1[[],a2[] two sorted arrays find the unioun of them in O(M+N) //
-	       
+	      			 // Given a1[[],a2[] two sorted arrays find the unioun of them in O(M+N) //
+
+// Algorithm.
 	//   Iterate both i,j from to n,m.
-	//  if(a1[i]<a2[j]) add a1[i] and i++.
-	//  else if(a1[i]>a2[j]) add a1[i] and j++.
-	//  else add any a1[i] or a2[j] and i++,j++.
-	// push remaining elements in a1[],a2[].
-	// return vector.
+	//  if(a1[i]<a2[j]) add a1[i] to vector v1 and i++.
+	//  else if(a1[i]>a2[j]) add a1[i] to vector v1 and j++.
+	//  else add   a1[i] ,a2[j] to vector v1 and i++,j++.
+	// Add remaining elements of a1[],a2[] in vector.
+	//  now take another vector and store elmeents such that v[i-1]!=v[i].
+	// return new vector.
 
 #include<stdio.h> 
   
@@ -15,30 +17,36 @@
    n is the number of elements in arr2[] */
 int printUnion(int arr1[], int arr2[], int m, int n) 
 { 
-  int i = 0, j = 0; 
-  
-  while (i < m && j < n) 
-  { 
-    if (arr1[i] < arr2[j]) {
-      printf(" %d ", arr1[i]); 
-      i++;
-     }
-    else if (arr2[j] < arr1[i]) {
-      printf(" %d ", arr2[j++]); j++;
-	  }
-    else
-    { 
-      printf(" %d ", arr2[j++]); 
-      i++; 
-    } 
-  } 
-  
-  /* Print remaining elements of the larger array */
-  while(i < m) {
-   printf(" %d ", arr1[i]); i++;
-  }
-  while(j < n) {
-   printf(" %d ", arr2[j]); j++;}
+   vector<int> v;
+    int i=0,j=0;
+    while(i<n && j<m){
+        if(arr1[i]>arr2[j]){
+            v.push_back(arr2[j]);
+            j++;
+        }
+        else if(arr1[i]<arr2[j]){
+            v.push_back(arr1[i]);
+            i++;
+        }
+        else{
+            v.push_back(arr1[i]);
+            v.push_back(arr2[j]);
+            i++;
+            j++;
+        }
+    }
+    while(i<n) v.push_back(arr1[i++]);
+    while(j<m) v.push_back(arr2[j++]);
+    
+    vector<int> v1;
+    v1.push_back(v[0]);
+    for(int k=1;k<v.size();k++){
+        if(v[k-1]!=v[k]){
+            v1.push_back(v[k]);
+        }
+    }
+    return v1;
+    
 } 
   
 /* Driver program to test above function */
