887caa2360ec5f861c6c5795ba3cae657225ad04
GAUTHAMPANDALA <41846057+GAUTHAMPANDALA@users.noreply.github.com>
2020-08-13 13:59:54 +0530

    Update diifbetweenhourandminute.cpp

--- Mathematics/diifbetweenhourandminute.cpp
+++ Mathematics/diifbetweenhourandminute.cpp
@@ -1,5 +1,6 @@
 
 					// Difference between the angle hand and hour hand	//
+		// Formula:  ans = abs(((hr*60 +min)/2) -(6*min)). as 30 deg per hr and 1/2 per mmin for hr hand and 6 deg per minutte for min hand//
 
 
 
@@ -10,7 +11,7 @@ using namespace std;
 	int getAngle(double hr, double min)
 {
     while (min >= 60) min -= 60;
-    while (hr >= 12) hr -= 12;
+    while (hr >= 12) hr -= 12;		// for 24 hr format.
     double hrAngle = (hr*60 + min)/2;  //30 deg per hour, 1/2 deg per minute
     double minAngle = 6*min;           //6 deg per minute
     double diff = abs(hrAngle - minAngle);
