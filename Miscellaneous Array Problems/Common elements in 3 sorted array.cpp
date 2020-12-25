9762358c7b1e71b854dba1f713dde7f00111762b
GAUTHAMPANDALA <41846057+GAUTHAMPANDALA@users.noreply.github.com>
2020-12-25 18:32:30 +0530

    Add files via upload

---
+++ Miscellaneous Array Problems/Common elements in 3 sorted array.cpp
@@ -0,0 +1,62 @@
+				// There are 3 SORTED ARRAYS find the common elements in them //
+	
+ // Algorithm
+ // We are using same algorithm as Merge function in mergesort.
+ // Let the current element traversed in ar1[] be x, in ar2[] be y and in ar3[] be z
+//     If x, y and z are same, 
+//           we can simply print any of them as common element and move ahead in all three arrays.
+//     Else If x < y, 
+//          we can move ahead in ar1[] as x cannot be a common element.
+//     Else If x > z and y > z), 
+//         we can simply move ahead in ar3[] as z cannot be a common element.
+
+
+
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+
+void findCommon(int ar1[], int ar2[], int ar3[],int n1, int n2, int n3,)  
+{ 
+    
+    int i = 0, j = 0, k = 0; 						// Initialize starting indexes for ar1[], ar2[] and ar3[]      
+    while (i < n1 && j < n2 && k < n3 ) 
+    { 
+         // If x = y and y = z, print any of them and move ahead  in all arrays 
+         if (ar1[i] == ar2[j] && ar2[j] == ar3[k] ) 
+         {   
+		    cout << ar1[i] << " ";   
+			i++; j++; k++; 
+		 } 
+  
+        
+         else if (ar1[i] < ar2[j])        // x < y 
+             i++; 
+  
+         
+         else if (ar2[j] < ar3[k])        // y < z 
+             j++; 
+  
+       
+       
+         else                           // We reach here when x > y and y > z , i.e., z is smallest 
+             k++; 
+    } 
+} 
+  
+// Driver program to test above function 
+int main() 
+{ 
+    int ar1[] = {1, 5, 10, 20, 40, 80}; 
+    int ar2[] = {6, 7, 20, 80, 100}; 
+    int ar3[] = {3, 4, 15, 20, 30, 70, 80, 120}; 
+    
+    int n1 = sizeof(ar1)/sizeof(ar1[0]); 
+    int n2 = sizeof(ar2)/sizeof(ar2[0]); 
+    int n3 = sizeof(ar3)/sizeof(ar3[0]); 
+      
+  
+    cout << "Common Elements are "; 
+    findCommon(ar1, ar2, ar3,n1, n2, n3); 
+    return 0; 
+} 
